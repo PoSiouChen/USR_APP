198457d808ba5c99d6065c1f4f7989df3eec6cff
pollychen5911 <pollychen3911@gmail.com>
2024-08-11 17:30:48 +0800

    改以疊加UI的方式顯示相機畫面
    原本Texture會因螢幕大小不同而被壓縮

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVF6SmYwYi8vLy8vQUFBQm1VNEVLdDJTcmt2bW5UY0ZyMmx4RnoxZm1xTmpIcS9LeVcvVkV3MWRFN2VuVXRBQkxOdFUvclNlM1hyVDJZT0tyVXkwcTVsaG8xSlZaVDRtWFZxZFN3ZGZrRkdMMVVhUTJCb1NIUlhVQTAybm5BVzB1R05PU1EvNWd3Q1B4ODcybnNNU3grenBuQS81ZVluMW8ycGc3SXJJaHIzUFFwdElPaFhScVJOT3pmR2g0R3p0aHUvMExlZTdIU3NNbHI1dWxucEVYUW9pVFZJYmxGQWVkNi9qK3VCRVkxNVdZWkFEa1NxSHYwb1dyVjVNdStqaDdjT2tUd1VXOHlDcjN6NHdvQnVySlBJQzFXVGdMOVZZUWhCaFVua0NtUTNraXhKdHpFWGJWRDJzYStHREFTS0o0ODBNNkkvQXlUVFdlMS9FNU5rQWJPVlpDaUJZUExHNStjaVd6Rk5SdUIyWDVxaXg0Z1ppNGxVVFI0cVU=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
